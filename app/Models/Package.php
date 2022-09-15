25cba678b281ddb3795b2d19d0a8a1ef8a1eaafc
AhmadWaleed96 <ahmad.wal96@gmail.com>
2022-09-15 19:43:57 +0300

    123

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -7,16 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model
 {
-<<<<<<< HEAD
-    use HasFactory;
-=======
 
     use HasFactory;
 
-	// protected $with = ['user'];
-
-    // public function users(){
-    //     return $this->hasMany(User::class);
-    // }
->>>>>>> a23d6a3fbbcc5a49e41b94280d43dff6be776b7a
 }
